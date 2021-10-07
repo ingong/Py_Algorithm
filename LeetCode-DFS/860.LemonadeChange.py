6a9730680653345e770a110f414382b6e9166ea3
ingong <insong1216@gmail.com>
2021-10-07 11:55:58 +0900

    211006 수 Lemonade Change

---
+++ LeetCode-DFS/860.LemonadeChange.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five, ten = 0, 0
+        
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if five > 0:
+                    five -= 1
+                    ten += 1
+                else:
+                    return False
+            else:
+                if five > 0 and ten > 0:
+                    five -= 1
+                    ten -= 1
+                elif five > 2:
+                    five -= 3
+                else:
+                    return False
+        return True
